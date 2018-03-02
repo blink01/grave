bb41046577ba3b70e94725c55e8c1c81bc9816cd
Eugene Blink (blink01) <blink01@email.franklin.edu>
2018-03-01 20:30:28 -0600

    Adding Commit from work computer. I see the preview with all installed parts.

---
+++ app/src/main/java/franklin/register_graves2/History.java
@@ -0,0 +1,17 @@
+package franklin.register_graves2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Eugene on 3/1/2018.
+ */
+
+public class History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+    }
+}
