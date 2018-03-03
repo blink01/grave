8f74c20e52ccc46990847cd9239ad494c29555ed
Eugene Blink (blink01) <blink01@email.franklin.edu>
2018-03-02 21:24:35 -0600

    Adding Commit from work computer. I see the preview with all installed parts.

--- app/src/main/java/franklin/register_graves2/History.java
+++ app/src/main/java/franklin/register_graves2/History.java
@@ -1,7 +1,10 @@
 package franklin.register_graves2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Eugene on 3/1/2018.
@@ -13,5 +16,19 @@ public class History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        returnButton();
+
+    }
+
+    private void returnButton() {
+        Button retButton = (Button) findViewById(R.id.buttonBack22);
+        retButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentOne = new Intent( History.this, MainActivity.class);
+                startActivity(intentOne);
+            }
+        });
     }
 }
--- app/src/main/java/franklin/register_graves2/MainActivity.java
+++ app/src/main/java/franklin/register_graves2/MainActivity.java
@@ -1,7 +1,10 @@
 package franklin.register_graves2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        histButton();
+    }
+
+    private void histButton() {
+        Button historyButton = (Button) findViewById(R.id.historyButton);
+        historyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentHist = new Intent (MainActivity.this, History.class);
+                startActivity(intentHist);
+            }
+        });
     }
 
 }
