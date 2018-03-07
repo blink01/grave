74a801a700ddf314e38406574ba59172630b63c4
Nick Simonelli <simone07@email.franklin.edu>
2018-03-06 20:03:45 -0500

    Added forms for the New Submission and Registration forms.

--- app/src/main/java/franklin/register_graves2/Registration.java
+++ app/src/main/java/franklin/register_graves2/Registration.java
@@ -1,11 +1,13 @@
 package franklin.register_graves2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Registration extends AppCompatActivity {
 
@@ -15,13 +17,24 @@ public class Registration extends AppCompatActivity {
         setContentView(R.layout.activity_registration);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        returnButton();
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void returnButton() {
+        Button retButton = (Button) findViewById(R.id.return_home_from_reg);
+        retButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentOne = new Intent( Registration.this, MainActivity.class);
+                startActivity(intentOne);
             }
         });
     }
--- app/src/main/java/franklin/register_graves2/newSubmission.java
+++ app/src/main/java/franklin/register_graves2/newSubmission.java
@@ -1,13 +1,66 @@
 package franklin.register_graves2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
-public class newSubmission extends AppCompatActivity {
+public abstract class newSubmission extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Spinner spinner;
+    private static final String[] paths = {"item 1", "item 2", "item 3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_submission);
+        returnButton();
+
+//        spinner = (Spinner)findViewById(R.id.condition);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(newSubmission.this,
+//                android.R.layout.simple_spinner_item,paths);
+//
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter);
+//        spinner.setOnItemSelectedListener(this);
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.condition);
+//create a list of items for the spinner.
+        String[] items = new String[]{"1", "2", "three"};
+//create an adapter to describe how the items are displayed, adapters are used in several places in android.
+//There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+    }
+
+    private void returnButton() {
+        Button retButton = (Button) findViewById(R.id.return_home_from_submission);
+        retButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentOne = new Intent(newSubmission.this, MainActivity.class);
+                startActivity(intentOne);
+            }
+        });
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
     }
 }
